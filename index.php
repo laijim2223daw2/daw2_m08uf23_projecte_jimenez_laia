7078f206312ec445f741baf958e086c46850bef9
lajiiz <lajiiz@zend-lajiiz.fjeclot.net>
2023-04-20 17:31:01 +0200

    Primer commit index.php

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso a la base de datos LDAP</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <header class="text-center mt-5">
+            <h1>Bienvenido al acceso a la base de datos LDAP</h1>
+            <h4>DAW2 M08UF2 M08UF3</h4>
+            <p>Autora: lajiiz2122daw2</p>
+            <p>Correu: 15587087.clotfje.edu</p>
+            <p>Github: https://github.com/laijim2223daw2/zendldapProject.git</p>
+        </header>
+        <section class="text-center mt-5">
+            <p>Esta aplicación web te permite acceder a una base de datos LDAP utilizando PHP desplegado sobre un servidor de aplicaciones ZendServer. Aquí podrás visualizar, añadir, modificar y borrar usuarios.</p>
+            <p>Para comenzar, inicia sesión haciendo clic en el siguiente botón:</p>
+            <div class="text-center">
+                <a href="login.php" class="btn btn-primary">Iniciar sesión</a>
+            </div>
+        </section>
+    </div>
+</body>
+</html>
